8f8ddefa95cff6854db2c8ef676f6675506f1e1e
coding4lif3 <fed.monaco@gmail.com>
2020-05-24 01:03:06 +0200

    Dinamic Properties files and Maven Profiles in order to be able to change properties values based on build environment

--- src/main/java/HelloTDD.java
+++ src/main/java/HelloTDD.java
@@ -1,9 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 public class HelloTDD {
 
         public static void main(String[] args) {
 
-            System.out.println("Hello, World!");
-            sayHelloWithName("Federico");
+            ResourceBundle rb = ResourceBundle.getBundle("config");
+
+            System.out.println("Hello, World! " + rb.getString("name"));
+            String helloString = sayHelloWithName("Federico");
         }
 
         public static String sayHelloWithName(String myName) {
